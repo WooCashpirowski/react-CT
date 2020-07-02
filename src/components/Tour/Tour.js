34d6a8f865848c77fe6f6a412c93b984bd3dfe64
Woo Cashpirowski <cashpirowski@gmail.com>
2020-07-02 15:20:42 +0200

    initial commit

---
+++ src/components/Tour/Tour.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./tour.scss";
+
+export default class Tour extends Component {
+  render() {
+    return <div>Yo! Tour!</div>;
+  }
+}
---
+++ src/components/TourList/TourList.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import Tour from "../Tour/Tour";
+import "./tourlist.scss";
+
+export default class TourList extends Component {
+  render() {
+    return <div>Tours!</div>;
+  }
+}
